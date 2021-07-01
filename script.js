eb16995c7e6637f330763f691002d2d7200675ff
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2021-06-30 22:07:45 -0300

    Fixed the game

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ var bebidaAleatoria = myArray[Math.floor(Math.random() * myArray.length)]
 var tentativas = 3
 
 while (tentativas > 0) {
-  var chute = prompt("Adivinhe a bebida da Ambev que está na minha cabeça. O que tem na geladeira: "Lipton Limão", "Guaraná Antarctica", "Budweiser", "Skol Beats", "Chopp Brahma Black", "Do bem Água de coco", "Gatorade Tangerina", "Fusion", "Ama sem gás"])
-
+  var chute = prompt("Adivinhe a bebida da Ambev que está na minha cabeça. O que tem na geladeira: Lipton Limão, Guaraná Antarctica, Budweiser, Skol Beats, Chopp Brahma Black, Do bem Água de coco, Gatorade Tangerina, Fusion, Ama sem gás")
     if(bebidaAleatoria == chute) {
     alert("Acertou. Agora fica o incentivo para ler!")    
     break
